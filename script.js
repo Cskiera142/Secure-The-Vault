9c3922b207f011f65edee38aceeb22fcc4d6f925
Colton Skiera <cskiera142@gmail.com>
2023-05-04 12:41:42 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Step 1. Create String
+
+const message =
+  "You have received this message because you have been chosen to open an importan vault. Here is the secret combination:";
+
+// Step 2. Assign combo number
+const num1 = "1 + 4";
+const num2 = "16 - 3";
+const num3 = "5 * 4";
+
+// Step 3. Alert window
+alert(`${message}\n${num1}\n${num2}\n${num3}`);
